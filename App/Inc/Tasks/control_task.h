4b876b5f42b3a843778c0b71ffc82d7cbee1912f
Aryan Abbasgholitabaromran <119327119+aryanomran@users.noreply.github.com>
2025-11-10 11:16:54 +0100

    Implement high-level "Boss" drivers
    This commit adds the C++ classes that implement the high-level
    "Control Panel" interfaces (IPressureControl, ISolenoidValve,
    IPressureSensor).

--- App/Inc/Tasks/control_task.h
+++ App/Inc/Tasks/control_task.h
@@ -1,13 +1,61 @@
-//
-// Created by Aryan Abbasgholitabaromran on 08/11/2025.
-//
+/*
+ * Created by Aryan Abbasgholitabaromran
+ *
+ * This file defines the C++ class for the Pneumatic Control "Muscle" Task.
+ * This class encapsulates all the logic for obeying commands and
+ * controlling the hardware.
+ */
 
 #ifndef FIRMWARE_CONTROL_TASK_H
 #define FIRMWARE_CONTROL_TASK_H
 
+#include "cmsis_os.h"            // For FreeRTOS types
+#include "Tasks/task_commands.h" // For the command enum
 
-class control_task {
+//          HARDWARE INTERFACES
+// These are the "tools" this task needs to do the hardware interactions job.
+#include "Interfaces/PressureControl.h"
+#include "Interfaces/SolenoidValve.h"
 
+ // Boss here means ---> PressureRegulatorDriver
+class ControlTask {
+public:
+    /**
+     * @brief Constructs a new ControlTask object.
+     * @param pPressureRegulator  Pointer to the "Boss" for the regulator.
+     * @param pSystoleValve       Pointer to the "Boss" for the systole valve.
+     * @param pDiastoleValve      Pointer to the "Boss" for the diastole valve.
+     * @param commandQueue        The queue to LISTEN ON for commands.
+     */
+    ControlTask(PressureControl* pPressureRegulator,
+                SolenoidValve* pSystoleValve,
+                SolenoidValve* pDiastoleValve,
+                osMessageQueueId_t commandQueue);
+
+    /**
+     * @brief Creates and starts the FreeRTOS task.
+     * This is the function will be called from main.cpp.
+     */
+    void start();
+
+private:
+    /**
+     * @brief The main task loop
+     * This function contains the for(;;) loop.
+     */
+    void run();
+
+    /**
+     * @brief The static "trampoline" function for FreeRTOS.
+     * @param argument A void pointer to the "this" instance of the class.
+     */
+    static void taskLauncher(void* argument);
+
+    // --- Private Member Variables (The "Tools") ---
+    PressureControl* m_pPressureRegulator;
+    SolenoidValve* m_pSystoleValve;
+    SolenoidValve* m_pDiastoleValve;
+    osMessageQueueId_t m_commandQueue;
 };
 
 
--- App/Src/Tasks/control_task.cpp
+++ App/Src/Tasks/control_task.cpp
@@ -1,5 +1,115 @@
-//
-// Created by Aryan Abbasgholitabaromran on 08/11/2025.
-//
+/*
+ * Created by Aryan Abbasgholitabaromran
+ *
+ * This file implements the logic for the ControlTask C++ class.
+ */
 
-#include "../../Inc/Tasks/control_task.h"
+
+#include "Tasks/control_task.h"
+
+/**
+ * @brief This is the constructor.
+ *
+ * It "initializes" the class by saving all the
+ * hardware interfaces and queue will need.
+ */
+ControlTask::ControlTask(PressureControl* pPressureRegulator,
+                         SolenoidValve* pSystoleValve,
+                         SolenoidValve* pDiastoleValve,
+                         osMessageQueueId_t commandQueue)
+        : m_pPressureRegulator(pPressureRegulator),
+          m_pSystoleValve(pSystoleValve),
+          m_pDiastoleValve(pDiastoleValve),
+          m_commandQueue(commandQueue)
+{
+
+}
+
+/**
+ * @brief Creates and starts the FreeRTOS task.
+ */
+void ControlTask::start()
+{
+    // 1. Define the attributes for our RTOS task
+    const osThreadAttr_t task_attributes = {
+            .name = "ControlTask",
+            .stack_size = 256 * 4,
+            .priority = (osPriority_t) osPriorityNormal, // Normal priority
+    };
+
+    // 2. Create the new FreeRTOS thread.
+    //    Pass a pointer to *this* C++ object instance as the argument.
+    osThreadNew(ControlTask::taskLauncher, this, &task_attributes);
+}
+
+/**
+ * @brief The static "trampoline" function for FreeRTOS.
+ *
+ * Its one taks is to "launch" the C++ "run" method.
+ */
+void ControlTask::taskLauncher(void* argument)
+{
+    // 1. Cast the void* argument back into a pointer to our class
+    ControlTask* taskInstance = static_cast<ControlTask*>(argument);
+
+    // 2. Call the real C++ "run" method
+    taskInstance->run();
+}
+
+/**
+ * @brief The main task loop (the private "job").
+ *
+ * This is the real, C++ member function that contains the task's
+ * infinite loop.
+ */
+void ControlTask::run()
+{
+    PneumaticCommand_t cmd; // A variable to hold the received command
+
+    // This is the main loop for this task
+    for(;;)
+    {
+        // 1. Wait forever until a message arrives in the queue.
+        osStatus_t status = osMessageQueueGet(m_commandQueue, &cmd, NULL, osWaitForever);
+
+        if (status == osOK) // message = Yes
+        {
+            // 2. Act on the command
+            switch (cmd)
+            {
+                case CMD_SYSTOLE_START:
+                {
+                    // --- SYSTOLE LOGIC ---
+
+                    // 1. Set the pressure regulator to 1.2 Bar (this is for now then I change it i a way that we can connect it to the GUI)
+
+                    m_pPressureRegulator->setPressure(1.2f);
+
+                    // 2. Close the vacuum valve first (safety)
+                    m_pDiastoleValve->deactivate();
+
+                    // 3. Open the pressure valve
+                    m_pSystoleValve->activate();
+
+                    break;
+                }
+
+                case CMD_DIASTOLE_START:
+                {
+                    // --- DIASTOLE LOGIC ---
+
+                    // 1. Set the pressure regulator to 0 Bar
+                    m_pPressureRegulator->setPressure(0.0f);
+
+                    // 2. Close the pressure valve first (safety)
+                    m_pSystoleValve->deactivate();
+
+                    // 3. Open the vacuum valve
+                    m_pDiastoleValve->activate();
+
+                    break;
+                }
+            }
+        }
+    }
+}
